6c56203fbbe7deb8473511a1b2d03d00506c7c35
Erik Gassler <erikgassler@live.com>
2022-08-12 07:24:02 -0700

    Add ImplicitType class to use instead of enums for implicit conversions to numeric values

---
+++ StoicDreams.BuildTests/DataTypes/ImplicitTypeTests.cs
@@ -0,0 +1,65 @@
+﻿namespace StoicDreams.Core.DataTypes;
+
+public class ImplicitTypeTests : TestFramework
+{
+	[Theory]
+	[InlineData(8, 9, 7)]
+	[InlineData((byte)8, 9, 7)]
+	[InlineData((char)8, 9, 7)]
+	[InlineData((short)8, 9, 7)]
+	[InlineData((long)8, 9, 7)]
+	public void Verify_Conversions_With_Bit_AND<TBaseType>(TBaseType input, TBaseType bitEquals, TBaseType bitUnequals)
+		where TBaseType : struct
+	{
+		ImplicitType<TBaseType> value = input;
+		TBaseType converted = value;
+		Assert.Equal(input, converted);
+
+		ImplicitType<TBaseType> impEquals = bitEquals;
+		ImplicitType<TBaseType> impUnequals = bitUnequals;
+		ImplicitType<TBaseType> bEqual = value & impEquals;
+		ImplicitType<TBaseType> bUnequal = value & impUnequals;
+		int resultEquals = Convert.ToInt32(bEqual.Value);
+		int resultUnequals = Convert.ToInt32(bUnequal.Value);
+		Assert.NotEqual(0, resultEquals);
+		Assert.Equal(0, resultUnequals);
+	}
+
+	[Theory]
+	[InlineData(8, 9, 9)]
+	[InlineData((byte)8, 3, 11)]
+	[InlineData((char)8, 4, 12)]
+	[InlineData((short)8, 7, 15)]
+	[InlineData((long)8, 1, 9)]
+	public void Verify_Conversions_With_Bit_OR<TBaseType>(TBaseType input, TBaseType bitOr, int bitResult)
+		where TBaseType : struct
+	{
+		ImplicitType<TBaseType> value = input;
+		TBaseType converted = value;
+		Assert.Equal(input, converted);
+
+		ImplicitType<TBaseType> impOr = bitOr;
+		ImplicitType<TBaseType> impResult = value | impOr;
+		int resultEquals = Convert.ToInt32(impResult.Value);
+		Assert.Equal(bitResult, resultEquals);
+	}
+
+	[Theory]
+	[InlineData(8, 9, 1)]
+	[InlineData((byte)8, 3, 11)]
+	[InlineData((char)8, 4, 12)]
+	[InlineData((short)8, 7, 15)]
+	[InlineData((long)8, 1, 9)]
+	public void Verify_Conversions_With_Bit_XOR<TBaseType>(TBaseType input, TBaseType bitOr, int bitResult)
+		where TBaseType : struct
+	{
+		ImplicitType<TBaseType> value = input;
+		TBaseType converted = value;
+		Assert.Equal(input, converted);
+
+		ImplicitType<TBaseType> impOr = bitOr;
+		ImplicitType<TBaseType> impResult = value ^ impOr;
+		int resultEquals = Convert.ToInt32(impResult.Value);
+		Assert.Equal(bitResult, resultEquals);
+	}
+}
---
+++ StoicDreams.Core/DataTypes/ImplicitType.cs
@@ -0,0 +1,68 @@
+﻿namespace StoicDreams.Core.DataTypes;
+
+public class ImplicitType<TBaseType>
+	where TBaseType : struct
+{
+	public TBaseType Value { get; set; } = default;
+
+	public static implicit operator TBaseType(ImplicitType<TBaseType> input) => input.Value;
+	public static implicit operator ImplicitType<TBaseType>(TBaseType input) => new() { Value = input };
+	public static ImplicitType<TBaseType> operator &(ImplicitType<TBaseType> input, ImplicitType<TBaseType> other)
+	{
+		{ if (input.Value is int a && other.Value is int b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(a & b); } }
+		{ if (input.Value is byte a && other.Value is byte b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(byte)(a & b); } }
+		{ if (input.Value is char a && other.Value is char b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(char)(a & b); } }
+		{ if (input.Value is short a && other.Value is short b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(short)(a & b); } }
+		{ if (input.Value is long a && other.Value is long b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(a & b); } }
+		throw new NotImplementedException("Unexpected type used for bit AND conversion");
+	}
+	public static ImplicitType<TBaseType> operator |(ImplicitType<TBaseType> input, ImplicitType<TBaseType> other)
+	{
+		{ if (input.Value is int a && other.Value is int b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(a | b); } }
+		{ if (input.Value is byte a && other.Value is byte b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(byte)(a | b); } }
+		{ if (input.Value is char a && other.Value is char b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(char)(a | b); } }
+		{ if (input.Value is short a && other.Value is short b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(short)(a | b); } }
+		{ if (input.Value is long a && other.Value is long b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(a | b); } }
+		throw new NotImplementedException("Unexpected type used for bit AND conversion");
+	}
+	public static ImplicitType<TBaseType> operator ^(ImplicitType<TBaseType> input, ImplicitType<TBaseType> other)
+	{
+		{ if (input.Value is int a && other.Value is int b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(a ^ b); } }
+		{ if (input.Value is byte a && other.Value is byte b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(byte)(a ^ b); } }
+		{ if (input.Value is char a && other.Value is char b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(char)(a ^ b); } }
+		{ if (input.Value is short a && other.Value is short b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(short)(a ^ b); } }
+		{ if (input.Value is long a && other.Value is long b) { return (ImplicitType<TBaseType>)(TBaseType)(object)(a ^ b); } }
+		throw new NotImplementedException("Unexpected type used for bit AND conversion");
+	}
+}
+
+public class ImplicitInt : ImplicitType<int>
+{
+	public static ImplicitInt operator &(ImplicitInt input, ImplicitInt other) => new() { Value = input.Value & other.Value };
+	public static ImplicitInt operator |(ImplicitInt input, ImplicitInt other) => new() { Value = input.Value | other.Value };
+	public static ImplicitInt operator ^(ImplicitInt input, ImplicitInt other) => new() { Value = input.Value ^ other.Value };
+}
+public class ImplicitByte : ImplicitType<byte>
+{
+	public static ImplicitByte operator &(ImplicitByte input, ImplicitByte other) => new() { Value = (byte)(input.Value & other.Value) };
+	public static ImplicitByte operator |(ImplicitByte input, ImplicitByte other) => new() { Value = (byte)(input.Value | other.Value) };
+	public static ImplicitByte operator ^(ImplicitByte input, ImplicitByte other) => new() { Value = (byte)(input.Value ^ other.Value) };
+}
+public class ImplicitChar : ImplicitType<char>
+{
+	public static ImplicitChar operator &(ImplicitChar input, ImplicitChar other) => new() { Value = (char)(input.Value & other.Value) };
+	public static ImplicitChar operator |(ImplicitChar input, ImplicitChar other) => new() { Value = (char)(input.Value | other.Value) };
+	public static ImplicitChar operator ^(ImplicitChar input, ImplicitChar other) => new() { Value = (char)(input.Value ^ other.Value) };
+}
+public class ImplicitShort : ImplicitType<short>
+{
+	public static ImplicitShort operator &(ImplicitShort input, ImplicitShort other) => new() { Value = (short)(input.Value & other.Value) };
+	public static ImplicitShort operator |(ImplicitShort input, ImplicitShort other) => new() { Value = (short)(input.Value | other.Value) };
+	public static ImplicitShort operator ^(ImplicitShort input, ImplicitShort other) => new() { Value = (short)(input.Value ^ other.Value) };
+}
+public class ImplicitLong : ImplicitType<long>
+{
+	public static ImplicitLong operator &(ImplicitLong input, ImplicitLong other) => new() { Value = input.Value & other.Value };
+	public static ImplicitLong operator |(ImplicitLong input, ImplicitLong other) => new() { Value = input.Value | other.Value };
+	public static ImplicitLong operator ^(ImplicitLong input, ImplicitLong other) => new() { Value = input.Value ^ other.Value };
+}
