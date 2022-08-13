ef503f73098a8371d1258f09fa94e05e6edff287
Erik Gassler <erikgassler@live.com>
2022-08-13 13:01:50 -0700

    Add HttpClient service to core

--- StoicDreams.Core/CoreStartup.cs
+++ StoicDreams.Core/CoreStartup.cs
@@ -4,6 +4,8 @@ public static class CoreStartup
 {
 	public static IServiceCollection AddStoicDreamsCore(this IServiceCollection services)
 	{
+		services.AddHttpClient();
+		services.AddTransient<HttpClient>(serviceProvider => serviceProvider.GetRequiredService<IHttpClientFactory>().CreateClient());
 		services.AddTransient<IApiRequest, ApiRequest>();
 		services.AddSingleton<IJsonConvert, JsonConvert>();
 		return services;
