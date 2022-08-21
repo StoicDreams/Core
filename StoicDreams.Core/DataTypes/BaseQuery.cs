64d183a907073d7a341b168c880999ebc1d8984a
Erik Gassler <erikgassler@live.com>
2022-08-21 10:33:14 -0700

    Add BaseQuery to use as base for Query objects between services

---
+++ StoicDreams.Core/DataTypes/BaseQuery.cs
@@ -0,0 +1,35 @@
+﻿namespace StoicDreams.Core.DataTypes;
+
+public class BaseQuery
+{
+	/// <summary>
+	/// Primary filter applied by System, not set from users.
+	/// </summary>
+	public string Filter { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Secondary Search filter to apply based on type of query, set from users.
+	/// </summary>
+	public string Search { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Current page number of result set
+	/// </summary>
+	public int Page { get; set; } = 1;
+
+	/// <summary>
+	/// Number of items per page.
+	/// </summary>
+	public int PerPage { get; set; } = 20;
+
+	/// <summary>
+	/// Start of index for which to include items from result set.
+	/// </summary>
+	public int StartIndex => (Page - 1) * PerPage;
+
+	/// <summary>
+	/// End of index for which to include items from result set.
+	/// Last included index number is EndIndex - 1.
+	/// </summary>
+	public int EndIndex => StartIndex + PerPage;
+}
