7796d170a163ae9be536908bc4ab1653568adfc8
Erik Gassler <erikgassler@live.com>
2022-08-23 10:02:53 -0700

    Add DataTransfer data type

---
+++ StoicDreams.Core/DataTypes/DataTransfer.cs
@@ -0,0 +1,26 @@
+﻿namespace StoicDreams.Core.DataTypes;
+
+/// <summary>
+/// Class used to transfer data to and from storage.
+/// </summary>
+public class DataTransfer
+{
+	/// <summary>
+	/// Depending on context, could be name of class type or category.
+	/// </summary>
+	public string DataType { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Raw data to store, encrypted and base64 encoded.
+	/// </summary>
+	public string Data { get; set; } = string.Empty;
+
+	public override string ToString() => $"{DataType}:{Data}";
+	public override int GetHashCode() => ToString().GetHashCode();
+	public override bool Equals(object? obj)
+	{
+		if (obj is string text) { return text == ToString(); }
+		if (obj is DataTransfer data) { return data.ToString() == ToString(); }
+		return false;
+	}
+}
