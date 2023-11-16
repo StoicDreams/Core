62fd56d0a3231281f8456b24fb9d9a889a2a4d44
Erik Gassler <erik@gassler.us>
2023-11-16 07:27:31 -0600

    Add folder detection to PS scripts

--- StoicDreams.Core/PowerShell/PostBuild.ps1
+++ StoicDreams.Core/PowerShell/PostBuild.ps1
@@ -4,9 +4,21 @@ $rgxTargetGetVersion = '<Version>(.+)</Version>'
 $rgxTargetXML = '<PackageReference Include="StoicDreams.Core" Version="([0-9\.]+)" />'
 Clear-Host;
 
+while (Test-Path './StoicDreams.Core') {
+	Set-Location './StoicDreams.Core'
+}
+
+if (!(Test-Path './PowerShell')) {
+	Set-Location ..
+}
+
+if (!(Test-Path './StoicDreams.Core.csproj')) {
+	throw "This script is expected to be run from the root of the StoicDreams.Core project."
+}
+
 Get-ChildItem -Path .\ -Filter *Core.csproj -Recurse -File | ForEach-Object {
 	$result = Select-String -Path $_.FullName -Pattern $rgxTargetGetVersion
-	if($result.Matches.Count -gt 0) {
+	if ($result.Matches.Count -gt 0) {
 		$version = $result.Matches[0].Groups[1].Value
 	}
 }
@@ -18,20 +30,20 @@ function UpdateProjectVersion {
 	)
 
 	$rgxTargetXML = '<PackageReference Include="StoicDreams.Core" Version="([0-9\.]+)" />'
-	$newXML = '<PackageReference Include="StoicDreams.Core" Version="'+$version+'" />'
+	$newXML = '<PackageReference Include="StoicDreams.Core" Version="' + $version + '" />'
 
-	if(!(Test-Path -Path $projectPath)) {
+	if (!(Test-Path -Path $projectPath)) {
 		Write-Host "Not found - $projectPath" -BackgroundColor Red -ForegroundColor White
 		return;
 	}
 	$content = Get-Content -Path $projectPath
 	$oldMatch = $content -match $rgxTargetXML
-	if($oldMatch.Length -eq 0) {
+	if ($oldMatch.Length -eq 0) {
 		#Write-Host "Doesn't use Core - $projectPath"
 		return;
 	}
 	$matches = $content -match $newXML
-	if($matches.Length -eq 1) {
+	if ($matches.Length -eq 1) {
 		Write-Host "Already up to date - $projectPath" -ForegroundColor Cyan
 		return;
 	}
@@ -40,24 +52,25 @@ function UpdateProjectVersion {
 	Write-Host "Updated - $projectPath" -ForegroundColor Green
 }
 
-if($version -ne $null) {
+if ($version -ne $null) {
 	Write-Host Found Version: $version -ForegroundColor Green
 	$rootpath = Get-Location
 	$rootpath = $rootpath.ToString().ToLower()
 	Write-Host Path: "Root Path Start: $rootpath"
-	while($rootpath.Contains('core')) {
+	while ($rootpath.Contains('core')) {
 		cd ..
 		$rootpath = Get-Location
 		$rootpath = $rootpath.ToString().ToLower()
 	}
 	Write-Host Path: "Root Path End: $rootpath"
-	$newXML = '<PackageReference Include="StoicDreams.Core" Version="'+$version+'" />'
+	$newXML = '<PackageReference Include="StoicDreams.Core" Version="' + $version + '" />'
 	Get-ChildItem -Path .\ -Filter *.csproj -Recurse -File | ForEach-Object {
 		UpdateProjectVersion $_.FullName $version
 	}
 	Get-ChildItem -Path .\ -Filter *README.md -Recurse -File | ForEach-Object {
 		UpdateProjectVersion $_.FullName $version
 	}
-} else {
+}
+else {
 	Write-Host Current version was not found -ForegroundColor Red
 }
--- StoicDreams.Core/PowerShell/PreBuild.ps1
+++ StoicDreams.Core/PowerShell/PreBuild.ps1
@@ -3,6 +3,18 @@
 $rgxTargetGetVersion = '<Version>([0-9]+)\.([0-9]+)\.([0-9]+)</Version>'
 Clear-Host;
 
+while (Test-Path './StoicDreams.Core') {
+	Set-Location './StoicDreams.Core'
+}
+
+if (!(Test-Path './PowerShell')) {
+	Set-Location ..
+}
+
+if (!(Test-Path './StoicDreams.Core.csproj')) {
+	throw "This script is expected to be run from the root of the StoicDreams.Core project."
+}
+
 $alphaversion = 1
 $betaversion = 0
 $rcversion = 0
@@ -10,7 +22,7 @@ $rcversion = 0
 Get-ChildItem -Path .\ -Filter *Core.csproj -Recurse -File | ForEach-Object {
 	Write-Host $_
 	$result = Select-String -Path $_.FullName -Pattern $rgxTargetGetVersion
-	if($result.Matches.Count -gt 0) {
+	if ($result.Matches.Count -gt 0) {
 		$alphaversion = [int]$result.Matches[0].Groups[1].Value
 		$betaversion = [int]$result.Matches[0].Groups[2].Value
 		$rcversion = [int]$result.Matches[0].Groups[3].Value
@@ -29,13 +41,15 @@ function CheckIfAnyFilesUpdated {
 			$lastupdate = Get-Content -Path $file
 			$lastupdate = [DateTime]::ParseExact($lastupdate, 'M/dd/yyyy h:mm:ss tt', [cultureinfo]'en-US')
 			Write-Host "Extracted last update: $lastupdate"
-		} catch {
-			Write-Host "Last update failed to load from file, defaulting to past 24 hours"
-			$lastupdate=$(Get-Date).AddHours(-24)
 		}
-	} else {
+		catch {
+			Write-Host "Last update failed to load from file, defaulting to past 24 hours"
+			$lastupdate = $(Get-Date).AddHours(-24)
+		}
+	}
+ else {
 		Write-Host "Last update file not found, defaulting to past 24 hours"
-		$lastupdate=$(Get-Date).AddHours(-24)
+		$lastupdate = $(Get-Date).AddHours(-24)
 	}
 	$currentUpdate = $lastupdate;
 	Write-Host "Loaded Last Update: $lastupdate"
@@ -46,7 +60,7 @@ function CheckIfAnyFilesUpdated {
 		$diff = $_.LastWriteTime - $lastupdate
 		# Need to account for small milliseconds discrepency not saved to file
 		if ($diff.TotalSeconds -lt 1) { return; }
-		Write-Host “File Name: ” + $_.FullName + "; Updated: " + $_.LastWriteTime
+		Write-Host ï¿½File Name: ï¿½ + $_.FullName + "; Updated: " + $_.LastWriteTime
 		if ($_.LastWriteTime -gt $currentUpdate) {
 			$currentUpdate = $_.LastWriteTime
 		}
@@ -76,20 +90,20 @@ function UpdateProjectVersion {
 	)
 
 	$rgxTargetXML = '<Version>([0-9\.]+)</Version>'
-	$newXML = '<Version>'+$version+'</Version>'
+	$newXML = '<Version>' + $version + '</Version>'
 
-	if(!(Test-Path -Path $projectPath)) {
+	if (!(Test-Path -Path $projectPath)) {
 		Write-Host "Not found - $projectPath" -BackgroundColor Red -ForegroundColor White
 		return;
 	}
 	$content = Get-Content -Path $projectPath
 	$oldMatch = $content -match $rgxTargetXML
-	if($oldMatch.Length -eq 0) {
+	if ($oldMatch.Length -eq 0) {
 		#Write-Host "Doesn't use Core - $projectPath"
 		return;
 	}
 	$matches = $content -match $newXML
-	if($matches.Length -eq 1) {
+	if ($matches.Length -eq 1) {
 		Write-Host "Already up to date - $projectPath" -ForegroundColor Cyan
 		return;
 	}
@@ -98,11 +112,11 @@ function UpdateProjectVersion {
 	Write-Host "Updated   - $projectPath" -ForegroundColor Green
 }
 Write-Host Version: $version
-if($version -ne $null) {
+if ($version -ne $null) {
 	$rootpath = Get-Location
 	$rootpath = $rootpath.ToString().ToLower()
 
-	while($rootpath.Contains('Core')) {
+	while ($rootpath.Contains('Core')) {
 		cd ..
 		$rootpath = Get-Location
 		$rootpath = $rootpath.ToString().ToLower()
